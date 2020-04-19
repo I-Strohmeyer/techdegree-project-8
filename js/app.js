e3131b1eab14e2c0c45f2881f62fd45b9ea196ac
IAS <is@daypaio.com>
2020-04-19 02:52:42 +0200

    search func + refactor

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
-
 // API FETCH SETUP
 //_________________________
 
 //Variables
-
 let employees = [];
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=US`;
 // Get the modal
@@ -11,9 +9,8 @@ const modal = document.getElementById("employee-info-modal");
 const modalDetails = document.getElementById("employee-details");
 const closeButton = document.getElementById("close");
 
-
-// Get the tiles that opens the modal
-const tiles = document.getElementsByClassName("employee-tile")[0];
+// Keep track of which modal is currently open (default is the first)
+let currentModalIndex = 0;
 
 //employee container
 const empContainer = document.getElementById("employees");
@@ -52,7 +49,7 @@ function displayEmployees(employeeData) {
               <div class="info-wrapper">
                 <img src="${picture.medium}" alt="portrait" />
                 <div class="employees-text">
-                  <h2>${name.first} ${name.last}</h2>
+                  <h2 class="searchable">${name.first} ${name.last}</h2>
                   <span>${email}</span>
                   <span>${city}</span>
                 </div>
@@ -64,29 +61,6 @@ function displayEmployees(employeeData) {
 
     empContainer.innerHTML = employeeHtml;
 };
-
-
-
-function displayModal(index) {
-
-  //use object destructuring to make template literal cleaner
-  let { name, dob, phone, email, location: { city, street, state, country, postcode}, picture} = employees[index];
-  
-  let date = new Date(dob.date);
-
-  const modalHtml = `
-        <img src="${picture.large}" alt="profile-pic" />
-        <h2 class="modal-name">${name.first} ${name.last}</h2>
-        <p>${email}</p>
-        <p>${city}</p>
-        <hr>
-        <p>${phone}</p>
-        <p>${street.number} ${street.name}, ${state} ${postcode}</p>
-        <p>Birthday: ${date.getMonth()} / ${date.getDate()} / ${date.getFullYear()}</p>
-  `;
-  modal.style.display = "block";
-  modalDetails.innerHTML = modalHtml;
-}
 
 // MODAL SETUP & Event
 //___________________________
@@ -107,14 +81,6 @@ empContainer.addEventListener('click', event => {
   }
 });
 
-/* Get the <span> element that closes the modal
-//const closeButton = document.getElementById("close");
-
-// When the user clicks on (x), close the modal
-//closeButton.addEventListener('click', () => {
-  modal.style.display = "none";
-});*/
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
@@ -124,13 +90,78 @@ window.onclick = function(event) {
 
 modal.addEventListener('click', event => {
   const previous = document.getElementById('previous');
-  if (event.target.tagName === 'BUTTON')
-  
-    if (event.target === previous)
-      alert("I'm the left button")
+  if (event.target.tagName === 'BUTTON') {
+    if (event.target === previous) {
+
+      if (currentModalIndex <= 0)
+        currentModalIndex = employees.length - 1;
+      else
+        currentModalIndex = currentModalIndex - 1;
+
+    }
     else {
-      alert("I'm the right button")
-    }  
+
+      if (currentModalIndex == employees.length - 1)
+        currentModalIndex = 0;
+      else
+        currentModalIndex = currentModalIndex + 1;
+
+    }
+    console.log(currentModalIndex);
+    setModalContent(currentModalIndex);
+  }
 });
 
+// SHOWING THE MODAL
+//___________________________
 
+function setModalContent(index) {
+  currentModalIndex = parseInt(index, 10);
+
+  //use object destructuring to make template literal cleaner
+  let { name, dob, phone, email, location: { city, street, state, country, postcode}, picture} = employees[index];
+  
+  let date = new Date(dob.date);
+
+  const modalHtml = `
+        <img src="${picture.large}" alt="profile-pic" />
+        <h2 class="modal-name">${name.first} ${name.last}</h2>
+        <p>${email}</p>
+        <p>${city}</p>
+        <hr>
+        <p>${phone}</p>
+        <p>${street.number} ${street.name}, ${state} ${postcode}</p>
+        <p>Birthday: ${date.getMonth()} / ${date.getDate()} / ${date.getFullYear()}</p>
+  `;
+  
+  modalDetails.innerHTML = modalHtml;
+}
+
+function displayModal(index) {
+  setModalContent(index);
+  modal.style.display = "block";
+}
+
+// SEARCH FUNCTION
+
+function searchEmployees() {
+  //variables
+  let input = document.getElementById('search');
+  let searchCriteria = input.value.toLowerCase();
+  
+  let tiles = document.getElementsByClassName("employee-tile");
+
+  //loop through employees
+  let nameValue;
+  for (let i = 0; i < tiles.length; i++) {
+    const tile = tiles[i];
+
+    nameValue = tile.getElementsByClassName("searchable")[0].innerText.toLowerCase();
+    if( nameValue.indexOf(searchCriteria) > -1 ) {
+      tile.style.display = "block";
+    } else {
+      tile.style.display = "none";
+    }
+  }
+
+  };
